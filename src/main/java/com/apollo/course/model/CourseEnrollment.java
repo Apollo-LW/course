87b8e46c5969c089e2f712fc89fc2fa8e5994a96
MrMoon <abuamaramfreelance@gmail.com>
2020-12-28 02:19:12 +0200

    commit added date of creation for the enrollment request

--- src/main/java/com/apollo/course/model/CourseEnrollment.java
+++ src/main/java/com/apollo/course/model/CourseEnrollment.java
@@ -2,10 +2,14 @@ package com.apollo.course.model;
 
 import lombok.Data;
 
+import java.util.Calendar;
+import java.util.Date;
+
 @Data
 public class CourseEnrollment {
 
     private String courseId , userId;
+    private Date requestDateOfCreation = Calendar.getInstance().getTime();
     private EnrollmentType enrollmentType;
 
 }
