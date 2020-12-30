448172b18af87405d7b38c4569008bca22144130
hindnasser <48987233+hindnasser@users.noreply.github.com>
2020-12-30 20:31:15 +0200

    Add files via upload

---
+++ src/main/java/com/apollo/course/model/ChapterQuiz.java
@@ -0,0 +1,10 @@
+package com.apollo.course.model;
+
+import java.util.HashSet;
+
+public class ChapterQuiz {
+
+    private String chapterId;
+    private HashSet<String> chapterQuizzes;
+   
+}
---
+++ src/main/java/com/apollo/course/model/Quiz.java
@@ -0,0 +1,20 @@
+package com.apollo.course.model;
+
+import java.util.*;
+
+public class Quiz {
+
+    private final String quizId = UUID.randomUUID().toString();
+    private final Date quizDateOfCreation = Calendar.getInstance().getTime();
+    private Date quizDueDate;
+    private String quizName = quizId + "-" + quizDateOfCreation;
+    private HashMap<String, String > quizQuestions = new HashMap<>();
+    private HashMap<String, List> quizAnswerOptions;
+    private HashMap<String, String> quizAnswer;
+    private String quizChapterId;
+    private String quizCourseId;
+    private String quizOwnerId;
+    private String quizDuration;
+
+
+}
