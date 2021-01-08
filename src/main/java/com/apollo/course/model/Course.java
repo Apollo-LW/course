e5ee82902701ee84f5899441ba857ed8a66fbb7b
mrmoon <abuamaramfreelance@gmail.com>
2021-01-08 21:39:14 +0200

    commit fix Course model

--- src/main/java/com/apollo/course/model/Course.java
+++ src/main/java/com/apollo/course/model/Course.java
@@ -9,7 +9,7 @@ public class Course {
 
     private boolean isPublic, isActive = true;
     private EnrollmentType courseEnrollmentType = EnrollmentType.PUBLIC;
-    private String courseId =  UUID.randomUUID().toString();
+    private String courseId =  UUID.randomUUID().toString() , roomId = "";
     private HashSet<Chapter> courseChapters = new HashSet<>();
     private Date courseDateOfCreation = Calendar.getInstance().getTime();
     private HashSet<String> courseOwners = new HashSet<>() , courseMembers = new HashSet<>();
